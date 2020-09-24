bf2f496bfd46d98a3a56a4dfc5770533d58e4e81
Georg Bremer <github@dschoordsch.de>
2020-09-24 12:08:41 +0200

    Boil down the problem to styles being updated via properties
    LogRocket does not update the style when it is dependent on properties
    or state and changed later (see background color of side panel).

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ to run it, you need a LogRocket project
 ngrok http 3000
 REACT_APP_LOG_ROCKET_APP_ID=<your/logrocket/id> yarn start
 ```
+
+Problem:
+
+LogRocket does not update the style if it is dependent on properties or state and updated later, see src/routes/index.tsx
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { Box, useMediaQuery } from '@material-ui/core';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { Box, Button, Drawer } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-import { SpaceDrawer } from 'components/sidePanel/SpaceDrawer';
-import { NavBar } from 'components/navbar/NavBar';
+interface StyleProps {
+  open: boolean;
+}
 
 const useStyles = makeStyles({
   spaceRoot: {
@@ -11,75 +12,43 @@ const useStyles = makeStyles({
     overflow: 'auto',
     width: '100%',
     height: '100%',
-    '--navBarHeight': 'clamp(44px, calc(1.5vw + 35px), 64px)',
   },
-  spaceContainer: {
-    position: 'absolute',
+  containerSize: {
+    //width: (props: StyleProps) => (props.open ? `min(333px, 50vw)` : 0),
+    backgroundColor: (props: StyleProps) => (props.open) ? 'red' : 'yellow',
+  },
+  drawer: {
+    overflow: 'hidden',
+    flexShrink: 1,
+  },
+  drawerPaper: {
     top: 'var(--navBarHeight, 0)',
-    left: 0,
     bottom: 0,
-    right: 0,
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  spaceTimer: {
-    position: 'fixed',
-    zIndex: 10,
-    width: '100%',
-    justifyContent: 'center',
-    pointerEvents: 'none',
+    overflow: 'auto',
+    height: 'auto',
   },
 });
 
+
+
 export function AppRouter() {
-  const theme = useTheme();
-  const xsDownLayout = useMediaQuery(theme.breakpoints.down('xs'));
-
-  const [currentLeftMenu, setCurrentLeftMenu] = React.useState<'left1' | 'left2' | undefined>();
-  const [currentRightMenu, setCurrentRightMenu] = React.useState<'right1' | undefined>();
-
-  const classes = useStyles();
-
-  const onSetCurrentLeftMenu = React.useCallback(
-    (menu: 'left1' | 'left2' | undefined) => {
-      if (xsDownLayout) {
-        setCurrentRightMenu(undefined);
-      }
-      setCurrentLeftMenu(menu);
-    },
-    [xsDownLayout]
-  );
-
-  const onSetCurrentRightMenu = React.useCallback(
-    (menu: 'right1' | undefined) => {
-      if (xsDownLayout) {
-        setCurrentLeftMenu(undefined);
-      }
-      setCurrentRightMenu(menu);
-    },
-    [xsDownLayout]
-  );
-
-  React.useEffect(() => {
-    if (xsDownLayout) {
-      setCurrentLeftMenu(undefined);
-      setCurrentRightMenu(undefined);
-    }
-  }, [xsDownLayout]);
-
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+  const classes = useStyles({ open: drawerOpen });
   return (
       <Box className={classes.spaceRoot}>
-        <NavBar
-          currentLeftMenu={currentLeftMenu}
-          setCurrentLeftMenu={onSetCurrentLeftMenu}
-          currentRightMenu={currentRightMenu}
-          setCurrentRightMenu={onSetCurrentRightMenu}
-        />
-        <Box className={classes.spaceContainer}>
-          <SpaceDrawer entries={['left1', 'left2']} currentMenu={currentLeftMenu} />
-          <Box>some content</Box>
-          <SpaceDrawer entries={['right1']} currentMenu={currentRightMenu} anchor="right" />
-        </Box>
+        <Drawer
+          open={drawerOpen}
+          variant="persistent"
+          className={`${classes.drawer} ${classes.containerSize}`}
+          classes={{
+            paper: `${classes.drawerPaper} ${classes.containerSize}`,
+          }}
+        >
+          SidePanel
+        </Drawer>
+        <Button onClick={() => {setDrawerOpen(!drawerOpen)}}>
+          {drawerOpen ? "Close drawer" : "Open drawer"}
+        </Button>
       </Box>
   );
 };
